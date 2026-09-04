55188d99ecb8e38868b244d57580fd5cb3e6598f
Manelisi1972 <st10447923@rcconnect.edu.za>
2026-09-04 14:40:44 +0200

    footer section

---
+++ includes/footer.php
@@ -0,0 +1,71 @@
+</main>
+
+<footer class="site-footer">
+
+    <div>
+
+        <h3>
+            GIANT AUTO UPHOLSTERY
+        </h3>
+
+        <p>
+            Premium upholstery and sofa repair services.
+        </p>
+
+    </div>
+
+    <div>
+
+        <h4>
+            Quick Links
+        </h4>
+
+        <a href="index.php">
+            Home
+        </a>
+
+        <a href="services.php">
+            Services
+        </a>
+
+        <a href="booking.php">
+            Book Now
+        </a>
+
+        <a href="contact.php">
+            Contact
+        </a>
+
+    </div>
+
+    <div>
+
+        <h4>
+            Contact
+        </h4>
+
+        <p>
+            Phone: 012 345 6789
+        </p>
+
+        <p>
+            Email: info@giantauto.co.za
+        </p>
+
+    </div>
+
+    <div class="copyright">
+
+        © <?= date('Y') ?>
+        Giant Auto Upholstery.
+        All Rights Reserved.
+
+    </div>
+
+</footer>
+
+<script src="assets/app.js"></script>
+
+</body>
+
+</html>
